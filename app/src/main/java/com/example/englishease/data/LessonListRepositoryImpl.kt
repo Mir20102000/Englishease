32d8436b55922baeacf8bf6ddd39b2959377e553
Mirziyod <radzapovm9@gmail.com>
2023-04-30 16:20:01 +0300

    Added in data packet object class LessonListRepositoryImpl

---
+++ app/src/main/java/com/example/englishease/data/LessonListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.englishease.data
+
+import com.example.englishease.domain.LessonItem
+import com.example.englishease.domain.LessonListRepository
+
+object LessonListRepositoryImpl: LessonListRepository {
+    override fun addLessonItem(lessonItem: LessonItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteLessonItem(lessonItem: LessonItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editLessonItem(lessonItem: LessonItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLessonItem(lessonItemId: Int): LessonItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLessonList(): List<LessonItem> {
+        TODO("Not yet implemented")
+    }
+}
